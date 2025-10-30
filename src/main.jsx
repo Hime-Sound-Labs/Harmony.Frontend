6466027b2dac18dc05c3886d4648ceae6bc5012f
RFX <rfxmusic@gmail.com>
2025-10-30 18:14:35 +0100

    Fix links and adjust routing to be less ugly

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,7 @@ createRoot(document.getElementById('root')).render(
     scaling="100%"
     appearance="dark"
   >
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </Theme>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,15 @@
 // This Source Code Form is "Incompatible With Secondary Licenses", as
 // defined by the Mozilla Public License, v. 2.0.
 
-import {Text, Flex, Link, Card, Grid} from "@radix-ui/themes";
+import {Text, Flex, Card, Grid} from "@radix-ui/themes";
 import './Home.css'
+import {Link} from "react-router";
 
 function Home() {
   return (
     <Flex justify="center" align="center" width="100%" height="80vh" className="bg-gradient">
       <Grid columns="2" gap="9"  maxWidth="60vw" justify="center">
-        <Link href="/harmony" className="link">
+        <Link to="/harmony" className="link">
           <Card size="3" className="card">
             <Flex justify="center" direction="column" gap="6">
               <Text as="p" size="5" align="center">
@@ -24,7 +25,7 @@ function Home() {
             </Flex>
           </Card>
         </Link>
-        <Link href="/tonundrum" className="link">
+        <Link to="/tonundrum" className="link">
           <Card size="3" className="card">
             <Flex justify="center" direction="column" gap="6">
               <Text as="p" size="5" align="center">
